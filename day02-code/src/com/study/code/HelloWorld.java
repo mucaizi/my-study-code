45c04fbddd544577fb3a7064d9e7a604fdf1684f
mucaizi <2827625657@qq.com>
2026-05-31 23:58:44 +0800

    测试

--- day02-code/src/com/study/code/HelloWorld.java
+++ day02-code/src/com/study/code/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String []args){
         String s = "32132";
         System.out.println("你好，世界");
+        System.out.println("github");
     }
 }
