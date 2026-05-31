1ffb4901c5b2545660a783bf6d6080f3da8eb8dc
mucaizi <2827625657@qq.com>
2026-06-01 00:01:40 +0800

    修改警告

--- day02-code/src/com/study/code/HelloWorld.java
+++ day02-code/src/com/study/code/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.study.code;
 
 public class HelloWorld {
-    public static void main(String []args){
+     static void main(String []args){
         String s = "32132";
-        System.out.println("你好，世界");
+
+        System.out.println("你好，世界" + s);
         System.out.println("github");
     }
 }
